c1d2e014eb663214661a31712a22e8a6ca4abd2b
Yanxin Lu <yanxin.lu@rice.edu>
2018-07-19 15:05:36 -0500

    Working on the script

--- batch_dom.sh
+++ batch_dom.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
-java -jar tool_files/maven_3_3_9/batch_dom_driver/target/batch_dom_driver-1.0-jar-with-dependencies.jar "$@"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: batch_dom.sh [dir path]"
+    echo ""
+    echo "Takes a directory containing Java programs and use batch dom driver on them"
+	exit 1
+fi 
+
+
+echo "Merging..."
+find $1 -type f -name "*.java.json" > "$1_json.txt"
+if [ ! -f "$1_json.txt" ]; then
+	echo "Failed to merge."
+	exit 1
+fi 
+python src/main/python/scripts/merge.py --output_file "$1.json" "$1_json.txt"
+exit 1
+
+if [ ! -d "$1" ]; then
+	echo "directory does not exist: $1"
+	exit 1
+fi 
+
+echo "Generating file list..."
+find $1 -type f -name "*.java" > "$1.txt"
+if [ ! -f "$1.txt" ]; then
+	echo "Failed to generate file list..."
+	exit 1
+fi 
+
+echo "Generating config file..."
+python prepare_conf.py $1
+if [ ! -f "$1.conf" ]; then
+	echo "Failed to generate config file..."
+	exit 1
+fi 
+
+echo "Running dom driver..."
+java -jar tool_files/maven_3_3_9/batch_dom_driver/target/batch_dom_driver-1.0-jar-with-dependencies.jar "$1.txt" "$1.conf"
---
+++ prepare_conf.py
@@ -0,0 +1,65 @@
+import os;
+import re;
+import sys;
+import json;
+
+def get_package(fname):
+    """
+    get the package this java file is in
+    """
+    lines = open(fname).read().split("\n");
+    for line in lines:
+        m = re.search('package\s+(.+?);', line);
+        if m is not None:
+            package = m.group(1);
+            return package;
+
+def gen_conf(pkg_set):
+    """
+    generate a bayou config using all the package names we gathered.
+    """
+
+    # read the config template
+    with open("conf.json", "r") as infile:
+        json_txt = infile.read();
+        conf_json = json.loads(json_txt);
+
+    for pkg in pkg_set:
+        if not pkg.startswith("edu.rice"):
+            conf_json["api-packages"].append(pkg);
+
+    # convert the json back to string
+    return json.dumps(conf_json, indent = 2);
+
+def main(args):
+    if len(args) < 2:
+        sys.exit("""Usage: prepare_conf.py [dir name]
+
+        This script is used to prepare a config file for running dom driver.
+        It takes a dir name and generate a dom driver config file filled with package names.
+        """);
+
+    dirname = args[1];
+    print "Generating config file for dir '%s'" % dirname;
+    all_pkg = set();
+
+    for dirpath, dirnames, filenames in os.walk(dirname):
+        for fname in filenames:
+            if fname.endswith(".java"):
+                pkg = get_package(dirpath + "/" + fname);
+                # print "package: '%s' from '%s'" %(pkg, fname);
+                all_pkg.add(pkg);
+    
+    print "Preparing config file..";
+    conf_str = gen_conf(all_pkg);
+    print conf_str;
+
+    out_fname = dirname + ".conf";
+    print "Writing config into '%s'" % out_fname;
+    with open(out_fname, "w") as outfile:
+        outfile.write(conf_str);
+
+    print "Done.";
+    
+if __name__ == '__main__':
+    main(sys.argv)
